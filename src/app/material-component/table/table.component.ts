47bf627434ecc2b83eead518425139d5f3e97b4d
Daniel Alvarez <daniel.alvarez@globaltank.eu>
2020-10-27 22:16:23 +0100

    prueba

--- src/app/material-component/table/table.component.ts
+++ src/app/material-component/table/table.component.ts
@@ -2,23 +2,23 @@ import { Component, OnInit } from '@angular/core';
 
 
 export interface PeriodicElement {
-  name: string;
+  id: string;
   position: number;
   weight: number;
   symbol: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 1, id: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, id: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, id: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, id: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, id: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, id: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, id: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, id: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, id: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, id: 'Neon', weight: 20.1797, symbol: 'Ne'},
 ];
 
 
